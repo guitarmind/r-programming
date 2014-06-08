dada29a8127b4ab07b724a922e0d13958837366e
markpeng <mark.peng@uitox.com>
2014-06-08 23:38:39 +0800

    Complete quiz 2.

--- Quiz/Quiz2/quiz2.R
+++ Quiz/Quiz2/quiz2.R
@@ -1,2 +1,46 @@
 # source("E:/Coursera/R_Programming/Quiz/Quiz1/quiz2.R")
 
+cube <- function(x, n) {
+  x^3
+}
+# cube(3)
+
+# x <- 1:10
+# if(x > 5) {
+#   x <- 0
+# }
+
+
+# f <- function(x) {
+#   g <- function(y) {
+#     y + z
+#   }
+#   z <- 4
+#   x + g(x)
+# }
+# z <- 10
+# print(f(3))
+
+
+# x <- 5
+# y <- if(x < 3) {
+#   NA
+# } else {
+#   10
+# }
+# print(y)
+
+h <- function(x, y = NULL, d = 3L) {
+  z <- cbind(x, d)
+  if(!is.null(y))
+    z <- z + y
+  else
+    z <- z + f
+  g <- x + y / z
+  if(d == 3L)
+    return(g)
+  g <- g + 10
+  g
+}
+
+
